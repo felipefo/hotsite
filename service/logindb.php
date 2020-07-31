1374a1cea79e0e9671c0e6dc190b9ad0669566c5
felipefo <felipefo@users.noreply.github.com>
2020-07-31 08:33:01 -0300

    .
    .

--- service/logindb.php
+++ service/logindb.php
@@ -1,7 +1,9 @@
 <?php
 
-    require "..\modelo\Usuario.php";
-	//require "../modelo/UsuarioDAO.php";
+   define('__ROOT__', dirname(dirname(__FILE__)));
+   require_once(__ROOT__ ."..\modelo\Usuario.php";);
+
+   //require "../modelo/UsuarioDAO.php";
 	echo "teste";
     try 
 	{
